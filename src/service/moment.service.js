cd2d7b626cc1e9d35dc5b3783d1a9cfd70b7ca0a
星霜下逆天生 <1486252015@qq.com>
2023-03-22 18:02:04 +0800

    fix(20-获取多条动态): 优化SQL语句

--- src/service/moment.service.js
+++ src/service/moment.service.js
@@ -1,5 +1,12 @@
 const connection = require('../app/database');
 
+const sqlFragment = `
+  SELECT
+    m.id id, m.content content,m.createAt createTime, m.updateAt updateTime,
+    JSON_OBJECT('id', u.id, 'name', u.name) user
+  FROM moment m
+  LEFT JOIN users u ON m.user_id = u.id`
+
 class MomentService {
   async create(userId, content){
    const statement =  `INSERT INTO moment (content, user_id) VALUES(?, ?)`;
@@ -9,11 +16,7 @@ class MomentService {
 
   async getMomentById(id) {
     const statement = `
-      SELECT
-        m.id id, m.content content,m.createAt createTime, m.updateAt updateTime,
-        JSON_OBJECT('id', u.id, 'name', u.name) user
-      FROM moment m
-      LEFT JOIN users u ON m.user_id = u.id
+      ${sqlFragment}
       WHERE m.id = ?;`;
     const [result] = await connection.execute(statement, [id]);
     return result[0];
@@ -22,11 +25,7 @@ class MomentService {
   async getMomentList(offset, size){
     console.log(offset)
     const statement = `
-      SELECT
-        m.id id, m.content content,m.createAt createTime, m.updateAt updateTime,
-        JSON_OBJECT('id', u.id, 'name', u.name) user
-      FROM moment m
-      LEFT JOIN users u ON m.user_id = u.id
+      ${sqlFragment}
       LIMIT?, ?;
     `;
     const [result] = await connection.execute(statement, [offset, size]);
